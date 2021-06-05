bbe5629d44e5ee213a648f4cf8fc9cf948f978ae
unknown <zayuto223@gmail.com>
2021-06-05 14:25:15 +0700

    first commit

--- resources/views/doktertambah0206.blade.php
+++ resources/views/doktertambah0206.blade.php
@@ -1,7 +1,19 @@
-<form action="{{ route ('dokter0206.store')}}" method="post">
-    @csrf
-    Id: <input type="text" name="id">
-    Nama: <input type="text" name="Nama">
-    Jabatan: <input type="text" name="Jabatan">
-    <button type="submit">simpan</button>
+<!DOCTYPE html>
+<html>
+<head>
+<title>1461900044</title>
+</head>
+<body>
+<h3>Tambah Pasien</h3>
+<a href="/pasien"> Kembali</a>
+<br/>
+<br/>
+<form action="/pasien/store" method="post">
+{{ csrf_field() }}
+Id <input type="text" name="id" required="required"> <br/>
+Nama <input type="text" name="nama" required="required"> <br/>
+Alamat <textarea name="alamat" required="required"></textarea> <br/>
+<input type="submit" value="Simpan Data">
 </form>
+</body>
+</html>
